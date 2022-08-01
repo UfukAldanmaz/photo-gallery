199ee638431ad5699c79eaa96e54daae3444f39a
UfukAldanmaz <61687476+UfukAldanmaz@users.noreply.github.com>
2022-08-01 11:45:41 +0300

    Add custom photo function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 const search = document.getElementById('search-input');
 let data = [];
+const form = document.getElementById('custom-form')
 
 search.addEventListener('keyup', (e) => {
     const searchText = e.target.value;
-    const filtered = data.filter((photo) => {
+    const filtered = data.filter(photo => {
         return (
             photo.title.toLowerCase().includes(searchText)
         );
@@ -17,38 +18,25 @@ search.addEventListener('keyup', (e) => {
 const displayPhotos = (data = []) => {
     const photos = document.querySelector('.photos');
 
+    while (photos.hasChildNodes()) {
+        photos.removeChild(photos.firstChild);
+    }
+
     data.map((photo) => {
-        const imgElement = document.createElement('div');
-        imgElement.classList.add('photo');
-        const imgTitle = document.createElement('h3');
-        imgTitle.classList.add('title');
-        const images = document.createElement('img');
-        images.classList.add('images')
-        const removeMark = document.createElement('span');
-        removeMark.classList.add('remove-mark');
-        removeMark.innerHTML = 'X'
-        removeMark.addEventListener('click', remove);
-        imgTitle.innerHTML = photo.title;
-        images.src = photo.thumbnailUrl;
-        imgElement.appendChild(removeMark);
-        imgElement.appendChild(imgTitle);
-        imgElement.appendChild(images);
-        photos.appendChild(imgElement);
+        const photoNode = createPhotoNode(photo);
+        photos.appendChild(photoNode);
     })
 }
 
-
 const getPhotos = async () => {
-
     try {
         const res = await fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
         data = await res.json()
         displayPhotos(data)
     } catch (error) {
-        console.error(err);
+        console.error(error);
     }
 }
-
 
 function remove() {
     document.querySelector('.photo').remove();
@@ -56,23 +44,36 @@ function remove() {
 
 getPhotos();
 
+const addCustomPhoto = (e) => {
+    e.preventDefault();
+    const photos = document.querySelector('.photos');
+    const title = document.getElementById('title').value;
+    const imageUrl = document.getElementById('img-url').value;
+    const photo = { title: title, thumbnailUrl: imageUrl }
+    const photoNode = createPhotoNode(photo);
+    photos.appendChild(photoNode);
+
+}
 
 
+form.addEventListener('submit', addCustomPhoto);
 
-// const search = () => {
-//     let input = document.getElementById('#search-input');
-//     let filter = input.value.toUpperCase();
-//     let div = document.querySelector('.photo');
-//     let h3;
-//     let textValue;
-//     for (let i = 0; i < data.length; i++) {
-//         h3 = div[i].getElementsByTagName('h3')[0];
-//         textValue = h3.textContent || h3.innerText;
-//         if (textValue.toUpperCase().indexOf(filter) > -1) {
-//             h3[i].style.display = "";
-//         } else {
-//             h3[i].style.display = "none";
-//         }
-//     }
-//     displayPhotos(textValue);
-// }
+const createPhotoNode = (photo) => {
+    const imgElement = document.createElement('div');
+    imgElement.classList.add('photo');
+    const imgTitle = document.createElement('h3');
+    imgTitle.classList.add('title');
+    const images = document.createElement('img');
+    images.classList.add('images')
+    const removeMark = document.createElement('span');
+    removeMark.classList.add('remove-mark');
+    removeMark.innerHTML = '❌'
+    removeMark.addEventListener('click', remove);
+    imgTitle.innerHTML = photo.title;
+    images.src = photo.thumbnailUrl;
+    imgElement.appendChild(removeMark);
+    imgElement.appendChild(imgTitle);
+    imgElement.appendChild(images);
+
+    return imgElement;
+}
